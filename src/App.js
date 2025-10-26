c64b5f694ed49ddbb8ce3adc4c30a559db9c9034
Henrique-Linhares <hlinhares453@gmail.com>
2025-10-26 15:07:55 -0300

    Implementado a page Formulario.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState, useEffect} from "react";
-import "./App.css"
-
-
-function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Menu/Sidebar.js';
+import Home from "./pages/Home/Home.js";
+import Formulario from './pages/Formulario/Formulario.js';
+import Lista from './pages/Lista/Lista';
+import './components/Menu/Sidebar.css'; 
+function App(){
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="layout-container">
+        <Sidebar /> 
+        
+        <main className="content-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lista" element={<Lista />} />
+            <Route path="/formulario" element={<Formulario />} />
+            <Route path="*" element={<div className="page-content"><h1>404: Página não encontrada</h1></div>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react"
+
+function Card({professor, setProfessor}) {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
